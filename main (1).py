b80257aa3970f00ad652b352951179ff53804257
paulamukai <122451048+paulamukai@users.noreply.github.com>
2023-01-12 12:04:06 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/entrar')
+def admin_index():
+  return render_template('login.html')
+
+@app.route('/login/', methods=['POST','GET'])
+def login():
+  if request.method == 'POST':
+    usuario = request.form['c_usuario']
+    senha = request.form['c_usuario']
+    if usuario == "paula" and senha == 456:
+      return redirect(url_for('admin', nome=usuario, senha=senha))
+    else:
+      return redirect(url_for('login'))
+  else:  
+    usuario = request.args.get('c_usuario')
+    senha = request.args.get('c_senha')
+    if usuario == "paula" and senha == 456:
+     return redirect(url_for('admin', nome=usuario, senha=senha))
+    else:
+     return redirect(url_for('login'))
+
+if __name__=='__main__':
+  app.run('0.0.0.0')
+
+
+@app.route('/admin/<nome>/<senha>')
+def admin(nome, senha):
+  frase = "<b>bem vindo</b>" + nome + "sua senha é": + senha + return frase
